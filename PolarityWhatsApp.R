d3ca583ff01881be69fb49da121f1fe2f32c64a6
Hrvoje Krpan <hrvoje.krpan@gmail.com>
2019-12-23 14:56:51 +0100

    Upload File

--- PolarityWhatsApp.R
+++ PolarityWhatsApp.R
@@ -1,10 +1,17 @@
+install.packages("magrittr")
 install.packages("xlsx")
 install.packages("qdap")
-install.packages("magrittr")
+install.packages("readxl")
 
 library(xlsx)
 library(qdap)
 library(magrittr)
+library("readxl")
+
+
+getwd()
+
+WAGypp <- read_excel("gyppWhatsApp.xlsx")
 
 #read data
 WAGypp <- read.xlsx("gyppWhatsApp.xlsx", sheetIndex = 1)
